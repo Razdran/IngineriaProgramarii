366ed67f96162d8e2c27c2131bf97ab291aebf08
Alex ln <logalex96@gmail.com>
2018-03-23 09:40:36 +0200

    Add IDEA

---
+++ IDE_classes/src/clase/Istoric.java
@@ -0,0 +1,25 @@
+package clase;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Istoric {
+
+  private static Istoric Instanta;
+  private static List<Comanda> comenzi;
+  private Istoric() {
+    comenzi = new ArrayList<Comanda>();
+
+  }
+  public static Istoric getInstance(){
+    if(Instanta == null)
+      Instanta = new Istoric();
+    return Instanta;
+  }
+  public List<Comanda> getIstoric(Integer id_utilizat) {
+    return comenzi;
+  }
+  public void setComanda(Comanda comanda){
+    comenzi.add(comanda);
+  }
+
+}
