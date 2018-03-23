6252622395468e55838de873c17982951867f089
Alex ln <logalex96@gmail.com>
2018-03-23 09:03:51 +0200

    Fix UML

---
+++ Generale clasele/Comanda.java
@@ -0,0 +1,31 @@
+import java.util.Vector;
+
+public class Comanda {
+
+  private Integer id_comanda;
+
+  private String status;
+
+  private List<String> continut;
+
+  private static Integer nextId;
+
+    public Vector  myIstoric;
+    public Vector  myIstoric;
+
+  public Integer get_id() {
+  return null;
+  }
+
+  public String get_status() {
+  return null;
+  }
+
+  public List<String> get_continut() {
+  return null;
+  }
+
+  public void set_id(Integer id) {
+  }
+
+}
---
+++ Generale clasele/Utilizator.java
@@ -0,0 +1,26 @@
+public class Utilizator {
+
+  private Integer id_utilizator;
+
+  private String nume;
+
+  private String adresa;
+
+    public Istoric myIstoric;
+
+  public Integer get_userID() {
+  return null;
+  }
+
+  public String get_nume() {
+  return null;
+  }
+
+  public String get_adresa() {
+  return null;
+  }
+
+  public void set_adresa(String adresa) {
+  }
+
+}
